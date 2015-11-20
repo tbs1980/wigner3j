19b6c414e0d5da4ba9b61b87d65c0087ef542401
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-11-20 14:53:24 +0000

    more structure added

--- tests/test_wigner3j.cpp
+++ tests/test_wigner3j.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <iomanip>
 #include <wigner3j.hpp>
 
 template<typename real_scalar_type>
 void test_wigner3j()
 {
-    long l2 = 4;
-    long l3 = 4;
-    long m2 = 4;
-    long m3 = 4;
+    long l2 = 2;
+    long l3 = 1;
+    long m2 = 0;
+    long m3 = 1;
     long m1 = -(m2+m3);
     long l1min =  std::max(std::abs(l2-l3),std::abs(m1));
     long l1max = l2+l3;
@@ -18,6 +19,12 @@ void test_wigner3j()
     std::cout<<"ndim = "<<ndim<<std::endl;
 
     std::vector<real_scalar_type> thrcof = wigner3j<real_scalar_type>(l2,l3,m2,m3);
+
+    std::cout<<"\nresuts"<<std::endl;
+    for(size_t i=0;i<thrcof.size();++i)
+    {
+        std::cout<<i<<"\t"<<std::setprecision(20)<<thrcof[i]<<std::endl;
+    }
 
 }
 
--- wigner3j.hpp
+++ wigner3j.hpp
@@ -8,16 +8,19 @@
 
 template<typename real_scalar_type>
 std::vector<real_scalar_type> wigner3j(
-    long const l2,
-    long const l3,
+    long const l2, // TODO should this be unsigned long?
+    long const l3, // TODO should this be unsigned long?
     long const m2,
     long const m3
 )
 {
-    long m1 = -(m2+m3);
-    long const l1min =  std::max(std::abs(l2-l3),std::abs(m1));
-    long const l1max = l2+l3;
-    long const ndim = l1max-l1min+1;
+    assert(std::abs(m2) <= l2);
+    assert(std::abs(m3) <= l3);
+
+    long m1 = -(m2 + m3);
+    long const l1min =  std::max(std::abs(l2-l3),std::abs(m1)); // TODO should this be unsigned long?
+    long const l1max = l2 + l3; // TODO should this be unsigned long?
+    long const ndim = l1max - l1min + 1; // TODO should this be unsigned long?
 
     std::vector<real_scalar_type> thrcof(ndim);
 
@@ -30,6 +33,112 @@ std::vector<real_scalar_type> wigner3j(
 
     long const one(1);
     long const two(2);
+
+    // If l1min=l1max, we have an analytical formula.
+    // TODO reference required here, e.g. an equation
+    if (l1min == l1max)
+    {
+        std::cout<<"we only have one coefficeit :)"<<std::endl;
+        thrcof[0] = std::pow(real_scalar_type(-1),std::abs(l2+m2-l3+m3))/std::sqrt( real_scalar_type(l1min+l2+l3+one) );
+    }
+    else
+    {
+        //std::cout<<"\n"<<std::endl;
+        // starting forward recursion from l1min taking nstep1 steps
+        long l1 = l1min;
+        //std::cout<<"l1 = "<<l1<<std::endl;
+        real_scalar_type newfac(0);
+        //std::cout<<"newfac = "<<newfac<<std::endl;
+        real_scalar_type c1(0);
+        //std::cout<<"c1 = "<<c1<<std::endl;
+        // We start with an arbitrary value.
+        thrcof[0] = srtiny;
+        //std::cout<<"thrcof[0] = "<<thrcof[0]<<std::endl;
+        real_scalar_type sum1 = real_scalar_type(two*l1 + one)*tiny;
+        //std::cout<<"sum1 = "<<sum1<<std::endl;
+
+        // recursion starts here
+        l1 = l1 + one;
+        //std::cout<<"l1 = "<<l1<<std::endl;
+        real_scalar_type oldfac = newfac;
+        //std::cout<<"oldfac = "<<oldfac<<std::endl;
+        real_scalar_type a1( (l1+l2+l3+one)*(l1-l2+l3)*(l1+l2-l3)*(-l1+l2+l3+one) );
+        //std::cout<<"a1 = "<<a1<<std::endl;
+        real_scalar_type a2( (l1+m1)*(l1-m1) );
+        //std::cout<<"a2 = "<<a2<<std::endl;
+        newfac = std::sqrt(a1*a2);
+        //std::cout<<"newfac = "<<std::setprecision(20)<<newfac<<std::endl;
+
+        assert(l1 != long(1)); // so that we check for if (l1.lt.one+eps) go to 40 in f90 version
+
+        real_scalar_type dv( -l2*(l2 + one)*m1 + l3*(l3 + one)*m1 + l1*(l1 - one)*(m3-m2) );
+        //std::cout<<"dv = "<<dv<<std::endl;
+        real_scalar_type denom = real_scalar_type(l1 - one)*newfac;
+        //std::cout<<"denom = "<<std::setprecision(20)<<denom<<std::endl;
+
+        c1 = -real_scalar_type(two*l1 - one)*dv/denom;
+        //std::cout<<"c1 = "<<std::setprecision(20)<<c1<<std::endl;
+
+        // if l1=l1min+1 the third term in the recursion eqn vanishes, hence
+        real_scalar_type x = srtiny*c1;
+        //std::cout<<"x = "<<std::setprecision(20)<<x<<std::endl;
+        thrcof[1] = x;
+        //std::cout<<"thrcof[1] = "<<std::setprecision(20)<<thrcof[1]<<std::endl;
+        sum1 = sum1+tiny*(two*l1 + one)*c1*c1;
+        //std::cout<<"sum1 = "<<std::setprecision(20)<<sum1<<std::endl;
+
+        if(ndim > 2)
+        {
+            l1 = l1 + one;
+            //std::cout<<"\nl1 = "<<l1<<std::endl;
+            oldfac = newfac;
+            //std::cout<<"oldfac = "<<oldfac<<std::endl;
+            a1 = real_scalar_type( (l1+l2+l3+one)*(l1-l2+l3)*(l1+l2-l3)*(-l1+l2+l3+one) );
+            //std::cout<<"a1 = "<<a1<<std::endl;
+            a2 = real_scalar_type( (l1+m1)*(l1-m1) );
+            //std::cout<<"a2 = "<<a2<<std::endl;
+            newfac = std::sqrt(a1*a2);
+            //std::cout<<"newfac = "<<std::setprecision(20)<<newfac<<std::endl;
+
+            assert(l1 != long(1)); // so that we check for if (l1.lt.one+eps) go to 40 in f90 version
+
+            dv = real_scalar_type( -l2*(l2 + one)*m1 + l3*(l3 + one)*m1 + l1*(l1 - one)*(m3-m2) );
+            //std::cout<<"dv = "<<dv<<std::endl;
+            denom = real_scalar_type(l1 - one)*newfac;
+            //std::cout<<"denom = "<<std::setprecision(20)<<denom<<std::endl;
+
+            real_scalar_type c1old = std::abs(c1);
+            //std::cout<<"c1old = "<<std::setprecision(20)<<c1old<<std::endl;
+
+            c1 = -real_scalar_type(two*l1 - one)*dv/denom;
+            //std::cout<<"c1 = "<<std::setprecision(20)<<c1<<std::endl;
+
+            real_scalar_type c2 = -l1*oldfac/denom;
+
+            // recursion to the next 3j-coeff x
+            x = c1*thrcof[1] + c2*thrcof[0];
+            //std::cout<<"x = "<<std::setprecision(20)<<x<<std::endl;
+            thrcof[2] = x;
+            //std::cout<<"thrcof[2] = "<<std::setprecision(20)<<thrcof[2]<<std::endl;
+            real_scalar_type sumfor = sum1;
+            //std::cout<<"sumfor = "<<std::setprecision(20)<<sumfor<<std::endl;
+            sum1 = sum1 + (two * l1 + one)*x*x;
+            //std::cout<<"sum1 = "<<std::setprecision(20)<<sum1<<std::endl;
+
+            // see if last unnormalised 3j-coeff exceeds srhuge
+            if (std::abs(x) < srhuge)
+            {
+                if (c1old-std::abs(c1))
+                {
+                    x1 = x;
+                    x2 = thrcof(lstep-1)
+                }
+            }
+        }
+    }
+
+
+
 
     return thrcof;
     /*
